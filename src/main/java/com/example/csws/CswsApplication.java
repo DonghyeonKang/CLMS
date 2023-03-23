0234531e3f2460b6acacdca07d2bd4906fe8bac9
DonghyeonKang <donghyeon3372@gmail.com>
2023-03-23 18:08:21 +0900

    create project

---
+++ src/main/java/com/example/csws/CswsApplication.java
@@ -0,0 +1,13 @@
+package com.example.csws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CswsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CswsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/csws/CswsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CswsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
