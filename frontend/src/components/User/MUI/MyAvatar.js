1d353e934f17270f72ab68b69f7485a690a3f103
woohyuk <wkdroal11@gmail.com>
2023-04-01 19:45:17 +0900

    Create MyAvatar.js

---
+++ frontend/src/components/User/MUI/MyAvatar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Avatar from "@mui/material/Avatar";
+
+const MyAvatar = () => {
+  return (
+    <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+    </Avatar>
+  );
+};
+
+export default MyAvatar;
