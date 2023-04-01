4e7119ab2a5661471daf3ad35939a386ca755a88
woohyuk <wkdroal11@gmail.com>
2023-04-01 19:43:40 +0900

    Update Router.js

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,7 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./routes/Main";
 import Login from "./routes/User/Login"
+import Admin from "./routes/Admin";
 import SignUp from "./routes/User/SignUp"
+import SignUpAd from "./routes/User/SignUpAd"
+import VerifyEmail from "./routes/User/VerifyEmail";
 import FindPw from "./routes/User/FindPw";
 import ChangePw from "./routes/User/ChangePw";
 import DashBoard from "./routes/Instance/DashBoard";
@@ -9,8 +12,7 @@ import InstanceDetail from "./routes/Instance/InstanceDetail";
 import CreateInstance from "./routes/Instance/CreateInstance";
 import SecurityGroup from "./routes/Instance/SecurityGroup";
 import InboundRules from "./routes/Instance/InboundRules";
-import ServerResources from "./routes/Server/ServerResources";
-import CreateServer from "./routes/Server/CreateServer";
+
 
 
 const Router = () => {
@@ -18,13 +20,13 @@ const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" exact element={<Main/>}/>
-
+          <Route path="/Admin" exact element={<Admin/>}/>
           <Route path="/login" exact element={<Login/>}/>
             <Route path="/login/signup" exact element={<SignUp/>}/>
+              <Route path="/login/signup/signupad" exact element={<SignUpAd/>}/>
             <Route path="/login/findpw" exact element={<FindPw/>}/>
               <Route path="/login/findpw/ChangePw" exact element={<ChangePw/>}/>
-          <Route path="/serverResources" exact element={<ServerResources/>}/>
-          <Route path="/createServer" exact element={<CreateServer/>}/>
+            <Route path="/login/VerifyEmail" exact element={<VerifyEmail/>}/>
           <Route path="/dashboard" exact element={<DashBoard/>}/>
             <Route path="/dashboard/:instanceId" exact element={<InstanceDetail/>}/> 
               <Route path="/dashboard/:instanceId/:securityGroupId" exact element={<SecurityGroup/>}/> 
