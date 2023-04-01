cde779e649a42d4104b7ec82c0cf62815c2d31f6
woohyuk <wkdroal11@gmail.com>
2023-04-01 19:44:44 +0900

    Create MyList.js

---
+++ frontend/src/components/Admin/MyList.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+
+export default function MyList(props) {
+  const [checked, setChecked] = React.useState([]);
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  return (
+    <List sx={{ width: '100%', maxWidth: 400, maxHeight: 500, overflow: 'auto', bgcolor: 'background.paper' }}>
+      {props.items.map((value, index) => {
+        const labelId = `checkbox-list-label-${index}`;
+
+        return (
+          <ListItem
+            key={index}
+            secondaryAction={
+              <IconButton edge="end" aria-label="comments"/>
+            }
+            disablePadding
+          >
+            <ListItemButton role={undefined} onClick={handleToggle(index)} dense>
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  checked={checked.indexOf(index) !== -1}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{ 'aria-labelledby': labelId }}
+                />
+              </ListItemIcon>
+              <ListItemText id={labelId} primary={`${value}`} />
+            </ListItemButton>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+}
