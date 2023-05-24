37747c8456beb3e13a05d577689799a786021c17
woohyuk <wkdroal11@gmail.com>
2023-05-24 18:48:01 +0900

    add comments
    코드 전반적 주석 추가

--- frontend/src/components/User/MUI/MyTextFieldUnivStu.js
+++ frontend/src/components/User/MUI/MyTextFieldUnivStu.js
@@ -9,7 +9,6 @@ const MyTextFieldUnivStu = (props) => {
   const [universities, setUniversities] = useState([]);
 
   useEffect(() => {
-    // 대학교 정보를 가져오는 API 요청
     axios.get(BASEURL+ '/register/universities')
       .then(response => {
         setUniversities(response.data.universities);
