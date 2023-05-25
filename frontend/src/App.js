9d53443939dc24ec4a58969e631747c04ab76ceb
woohyuk <wkdroal11@gmail.com>
2023-05-26 02:32:35 +0900

    Apdate Login JWT
    로그인 토큰 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import GlobalStyle from './GlobalStyle';
 import Router from './Router';
+import { useRecoilState } from 'recoil';
+import { useEffect } from 'react';
+import { loginState, tokenState } from './Atoms';
 
 const queryClient = new QueryClient();
 
 function App() {
+  const [, setLoginStatus] = useRecoilState(loginState);
+  const [, setToken] = useRecoilState(tokenState);
+
+  useEffect(() => {
+    const jwtToken = localStorage.getItem('jwt');
+    if (jwtToken) {
+      setToken(jwtToken);
+      setLoginStatus(true);
+    }
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <GlobalStyle/>
