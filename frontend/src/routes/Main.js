6efb0982d33392ad8b3d2d8ed28417d001398377
woohyuk <wkdroal11@gmail.com>
2023-06-05 20:50:31 +0900

    Update Banner
    배너 추가

--- frontend/src/routes/Main.js
+++ frontend/src/routes/Main.js
@@ -6,6 +6,9 @@ import "slick-carousel/slick/slick-theme.css";
 import Image1 from "../img/IMAGE1.png";
 import Image2 from "../img/IMAGE2.png";
 import Image3 from "../img/IMAGE3.png";
+import Image4 from "../img/IMAGE4.png";
+import Image5 from "../img/IMAGE5.png";
+import Footer from "../components/Footer";
 
 const Main = () => {
   const settings = {
@@ -16,7 +19,8 @@ const Main = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    initialSlide: 2
+    initialSlide: 2,
+    arrows: false,
   };
 
   return (
@@ -40,22 +44,38 @@ const Main = () => {
               src={Image3}
               alt="광고3"
             /></div>
+            <div>
+            <SlideImage 
+              src={Image4}
+              alt="광고4"
+            /></div>
+            <div>
+            <SlideImage 
+              src={Image5}
+              alt="광고5"
+            /></div>            
         </Slider>
         </SliderWrapper>
+
+        
       </Content>
+      <Footer/>
     </>
   );
 };
 
 const Content = styled.div`
   padding-top: 55px;
-  width: 97%;
+  width: 100%;
+  min-height: 80vh;
+  margin-bottom: 120px;
 `;
 const SlideImage = styled.img`
   width: 100%;
   height: auto;
 `;
 const SliderWrapper = styled.div`
-  margin-left: 50px;
+  width: 100%;
 `;
+
 export default Main;
