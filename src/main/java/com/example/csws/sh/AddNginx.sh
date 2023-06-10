aa488551b43fdabb233335220620296f62adaad1
truemanburbank <juxtap.se@gmail.com>
2023-06-10 16:16:23 +0900

    Update AddNginx.sh

--- src/main/java/com/example/csws/sh/AddNginx.sh
+++ src/main/java/com/example/csws/sh/AddNginx.sh
@@ -10,8 +10,8 @@ AddNginx()
     local serviceName=$3
     local domainName=$4
     local port=$5 
-    local ip=$6
-    ssh $hostName@$hostIp "bash ~/sh/H_AddNginx.sh $serviceName $domainName $port $ip"    
+
+    ssh $hostName@$hostIp "bash ~/sh/H_AddNginx.sh $serviceName $domainName $port $hostIp"    
 }
 Start AddNginx
-AddNginx $1 $2 $3 $4 $5 $6 && CSWSSuccess AddNginx || CSWSFailure AddNginx
+AddNginx $1 $2 $3 $4 $5 && CSWSSuccess AddNginx || CSWSFailure AddNginx
