6be155debe4be5509304865e25afa2da032a2fde
truemanburbank <juxtap.se@gmail.com>
2023-06-10 16:16:31 +0900

    Revert "Update AddNginx.sh"
    This reverts commit aa488551b43fdabb233335220620296f62adaad1.

--- src/main/java/com/example/csws/sh/AddNginx.sh
+++ src/main/java/com/example/csws/sh/AddNginx.sh
@@ -10,8 +10,8 @@ AddNginx()
     local serviceName=$3
     local domainName=$4
     local port=$5 
-
-    ssh $hostName@$hostIp "bash ~/sh/H_AddNginx.sh $serviceName $domainName $port $hostIp"    
+    local ip=$6
+    ssh $hostName@$hostIp "bash ~/sh/H_AddNginx.sh $serviceName $domainName $port $ip"    
 }
 Start AddNginx
-AddNginx $1 $2 $3 $4 $5 && CSWSSuccess AddNginx || CSWSFailure AddNginx
+AddNginx $1 $2 $3 $4 $5 $6 && CSWSSuccess AddNginx || CSWSFailure AddNginx
