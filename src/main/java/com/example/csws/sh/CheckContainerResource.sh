b289b4c772a2f45b8e675318bc3acdf31f301266
truemanburbank <juxtap.se@gmail.com>
2023-06-11 21:47:10 +0900

    출력 쉘 수정, Print 호스트쉘 삭제

--- src/main/java/com/example/csws/sh/CheckContainerResource.sh
+++ src/main/java/com/example/csws/sh/CheckContainerResource.sh
@@ -6,27 +6,13 @@ CheckContainerResource()
     local hostIp=$2 
     local container=$3
 
-    ssh $hostName@$hostIp "mkdir -p ~/etc/info/con"
-    ssh $hostName@$hostIp "touch ~/etc/info/con/$container.txt"
+    ssh $hostName@$hostIp "docker stats $container --no-stream --format 'table {{.MemUsage}}'"
+    ssh $hostName@$hostIp "echo =================================================================="
+    ssh $hostName@$hostIp "echo "Disk Usage""
+    ssh $hostName@$hostIp "docker ps --size --filter "name=$container" --format "{{.Size}}""
+    ssh $hostName@$hostIp "echo =================================================================="
+    ssh $hostName@$hostIp "docker stats $container --no-stream --format 'table {{.CPUPerc}}'"
 
-    ssh $hostName@$hostIp "docker stats $container --no-stream --format 'table {{.MemUsage}}' > ~/etc/info/con/$container.txt"
-    ssh $hostName@$hostIp "echo ================================================================== >> ~/etc/info/con/$container.txt"
-    ssh $hostName@$hostIp "echo "Disk Usage" >> ~/etc/info/con/$container.txt"
-    ssh $hostName@$hostIp "docker ps --size --filter "name=$container" --format "{{.Size}}" >> ~/etc/info/con/$container.txt"
-    ssh $hostName@$hostIp "echo ================================================================== >> ~/etc/info/con/$container.txt"
-    ssh $hostName@$hostIp "docker stats $container --no-stream --format 'table {{.CPUPerc}}' >> ~/etc/info/con/$container.txt"
-
-
-    # info 디렉토리가 존재하지 않으면 생성
-    if [ ! -d ~/info/$hostName/con ]; then
-        mkdir -p ~/info/$hostName/con
-    fi
-
-    if [ -f ~/info/con/$hostName/$container.txt ]; then
-        rm ~/info/$hostName/con/$container.txt
-    fi
-
-    scp $hostName@$hostIp:~/etc/info/con/$container.txt ~/info/$hostName/con/  
 }
 
 Start CheckContainerResource
--- src/main/java/com/example/csws/sh/PrintStatusforManager.sh
+++ src/main/java/com/example/csws/sh/PrintStatusforManager.sh
@@ -7,14 +7,7 @@ PrintStatusforManager()
     local hostName=$1
     local hostIp=$2 
 
-    ssh $hostName@$hostIp "sh ~/sh/H_PrintStatusforManager.sh" 
-
-    # hostName 디렉토리가 존재하지 않으면 생성
-    if [ ! -d ~/info/$hostName ]; then
-        mkdir -p ~/info/$hostName
-    fi
-
-    scp $hostName@$hostIp:~/etc/info/PrintStatusforManager.txt ~/info/$hostName/   
+    ssh $hostName@$hostIp "docker ps -a --format 'table {{.Names}}\t{{.Status}}\t'" 
 }
 Start PrintStatusforManager
 PrintStatusforManager $1 $2 && CSWSSuccess PrintStatusforManager || CSWSFailure PrintStatusforManager
