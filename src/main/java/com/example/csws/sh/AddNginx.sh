03fe0960608d8cb98857072165182e9a2d0cef70
truemanburbank <juxtap.se@gmail.com>
2023-06-12 21:17:57 +0900

    Nginx 도메인 관련 수정
    .com만으로 제한되지 않게 수정,
    conf 파일 이름을 컨테이너 이름으로 생성하게끔 수정

--- src/main/java/com/example/csws/sh/AddNginx.sh
+++ src/main/java/com/example/csws/sh/AddNginx.sh
@@ -7,10 +7,15 @@ AddNginx()
     local hostName=$1
     local hostIp=$2 
 
-    local domainName=$3
-    local port=$4 
+    local containerName=$3
+    local domain=$4
+    local port=$5 
 
-    ssh $hostName@$hostIp "bash ~/sh/H_AddNginx.sh $domainName $port $hostIp"    
+    ssh $hostName@$hostIp "bash ~/sh/H_AddNginx.sh $containerName $domain $port $hostIp"    
 }
 Start AddNginx
+<<<<<<< Updated upstream
 AddNginx $1 $2 $3 $4 && CSWSSuccess AddNginx || CSWSFailure AddNginx
+=======
+AddNginx $1 $2 $3 $4 $5 && CSWSSuccess AddNginx || CSWSFailure AddNginx
+>>>>>>> Stashed changes
