a2963222811bbfb392f949cec0becb2116ab52cd
truemanburbank <juxtap.se@gmail.com>
2023-06-12 18:07:06 +0900

    Nginx 인수 수정

--- src/main/java/com/example/csws/sh/AddNginx.sh
+++ src/main/java/com/example/csws/sh/AddNginx.sh
@@ -7,11 +7,10 @@ AddNginx()
     local hostName=$1
     local hostIp=$2 
 
-    local serviceName=$3
-    local domainName=$4
-    local port=$5 
-    local ip=$6
-    ssh $hostName@$hostIp "bash ~/sh/H_AddNginx.sh $serviceName $domainName $port $ip"    
+    local domainName=$3
+    local port=$4 
+
+    ssh $hostName@$hostIp "bash ~/sh/H_AddNginx.sh $domainName $port $hostIp"    
 }
 Start AddNginx
-AddNginx $1 $2 $3 $4 $5 $6 && CSWSSuccess AddNginx || CSWSFailure AddNginx
+AddNginx $1 $2 $3 $4 && CSWSSuccess AddNginx || CSWSFailure AddNginx
