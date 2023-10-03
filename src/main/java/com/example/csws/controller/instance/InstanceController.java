10cedac1dd187f43fede029ba729bc92c1bf4a65
DonghyeonKang <donghyeon3372@gmail.com>
2023-10-03 14:28:12 +0900

    fix cors

--- src/main/java/com/example/csws/controller/instance/InstanceController.java
+++ src/main/java/com/example/csws/controller/instance/InstanceController.java
@@ -179,6 +179,7 @@ public class InstanceController {
     // 본인 혹은 타인(관리자 권한)의 인스턴스 목록 조회(userId)
     @GetMapping("/list/user")  // 본인의 목록을 조회하면 userId가 null로 넘어온다. null을 허용하기 위한 어노테이션.
     public JSONObject listByUserId(Authentication authentication) {
+        System.out.println(authentication);
         // 로그인된 사용자 객체
         PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
 
